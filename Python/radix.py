2435b3a69ab6b897f0d9dbd05ed1b703c15dafbb
Filip <blazerwarrior@gmail.com>
2023-11-21 13:52:01 +0100

    Moved functions to a seperate class

--- Python/radix.py
+++ Python/radix.py
@@ -1,50 +1,6 @@
+from Sorter import Sorter
+
 order = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-
-#PL - Łączenie Kubełków z danymi w oparciu o order
-def join_buckets(buckets: list):
-    ret = []
-    for radix in order:
-        for el in buckets[radix]:
-            ret.append(el)
-    return ret
-
-#PL - Tworzy pusty kubełek
-def create_empty_buckets():
-    ret = {}
-    for radix in order:
-        ret[radix] = []
-    return ret
-
-#PL - Konwersja zawartośni na stringi
-def turn_to_str(arr: list):
-    ret = []
-    for el in arr:
-        ret.append(str(el))
-    return ret
-
-#PL - Odanezienie wartości z największą liczbą znaków i zwrócenie tego jako wartość liczbowa
-def get_biggest_length(arr: list):
-    biggest = 0
-    for el in arr:
-        if biggest < len(el):
-            biggest = len(el)
-    return biggest
-
-#PL - Dopisanie 0 z przodu w celu wyrównania ilości znaków w każdym elemencie
-def fill_array(arr: list, size: int):
-    ret = []
-    for el in arr:
-        temp = order[0] * (size - len(el)) + el
-        ret.append(temp)
-    return ret
-
-#PL - Konwersja zmiennej do wartości całkowitej
-def turn_to_int(arr: list):
-    ret = []
-    for el in arr:
-        ret.append(int(el))
-    return ret
 
 #PL - Pobranie danych od użytkownika z konsoli
 def get_input_from_console():
@@ -67,15 +23,9 @@ def get_input_from_console():
 #PL - Program główny w którym wykonane są wszystkie powyższe Funkcje
 def main():
     arr = get_input_from_console()
-    arr = turn_to_str(arr)
-    size = get_biggest_length(arr)
-    arr = fill_array(arr, size)
-    for i in range(1, size + 1):
-        buckets = create_empty_buckets()
-        for el in arr:
-            buckets[el[-1 * i]].append(el)
-        arr = join_buckets(buckets)
-    print(turn_to_int(arr))
+    sorter = Sorter(arr, order = order)
+    sorter.sort()
+    print(sorter.working_array)
 
 if __name__ == '__main__':
     main()
