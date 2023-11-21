f87fc96bc58d931b010f697ddfcfb98e812311aa
Filip <blazerwarrior@gmail.com>
2023-11-21 13:31:37 +0100

    Added breaks

--- Python/radix.py
+++ Python/radix.py
@@ -1,4 +1,6 @@
 order = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
 #PL - Łączenie Kubełków z danymi w oparciu o order
 def join_buckets(buckets: list):
     ret = []
@@ -6,18 +8,21 @@ def join_buckets(buckets: list):
         for el in buckets[radix]:
             ret.append(el)
     return ret
+
 #PL - Tworzy pusty kubełek
 def createEmptyBuckets():
     ret = {}
     for radix in order:
         ret[radix] = []
     return ret
+
 #PL - Konwersja zawartośni na stringi
 def turnToStr(arr: list):
     ret = []
     for el in arr:
         ret.append(str(el))
     return ret
+
 #PL - Odanezienie wartości z największą liczbą znaków i zwrócenie tego jako wartość liczbowa
 def get_biggest_length(arr: list):
     biggest = 0
@@ -25,6 +30,7 @@ def get_biggest_length(arr: list):
         if biggest < len(el):
             biggest = len(el)
     return biggest
+
 #PL - Dopisanie 0 z przodu w celu wyrównania ilości znaków w każdym elemencie
 def fill_array(arr: list, size: int):
     ret = []
@@ -32,12 +38,14 @@ def fill_array(arr: list, size: int):
         temp = order[0] * (size - len(el)) + el
         ret.append(temp)
     return ret
+
 #PL - Konwersja zmiennej do wartości całkowitej
 def turnToInt(arr: list):
     ret = []
     for el in arr:
         ret.append(int(el))
     return ret
+
 #PL - Program główny w którym wykonane są wszystkie powyższe Funkcje
 def main():
     arr = [505, 103, 87, 20, 325, 211, 134, 8]
